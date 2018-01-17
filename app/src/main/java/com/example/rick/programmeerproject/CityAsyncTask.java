7c81bb5a43c7a4c8c7a664a84f1765474411ad22
RickBakker01 <rickbakker01@gmail.com>
2018-01-17 13:17:02 +0100

    test commit

--- app/src/main/java/com/example/rick/programmeerproject/CityAsyncTask.java
+++ app/src/main/java/com/example/rick/programmeerproject/CityAsyncTask.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  * Created by Rick on 17-1-2018.
+ * test comment git
  */
 class CityAsyncTask extends AsyncTask<String, Integer, String> {
     private Context context;
